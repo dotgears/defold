9e3833677f9f4b869903c9204cea6e30f1eb2a51
TrungB <deulamco@gmail.com>
2020-08-29 14:18:33 -0400

    cleanup cmd.sh & improve to adapt ubuntu usage

--- cmd.sh
+++ cmd.sh
@@ -27,8 +27,7 @@ BUNDLE="sh bundle_editor.sh $2"
 SUB_MODULE="sh ./scripts/submodule.sh config_in_waf $2 $3 $4 $5 $6"
 WAF_CONF="(cd $3;PREFIX=\$DYNAMO_HOME waf configure --platform=$2)"
 
-COPY_ENGINE="sh cmd.sh --copy $2"
-COPY_ENGINE_X8664="sh cmd.sh --copy x86_64-darwin"
+COPY_ENGINE="./cmd.sh --copy $2"
 
 BUILD_ENGINE="sudo ./scripts/build.py build_engine --platform=$2 $3 $4 --skip-docs --skip-tests -- --skip-build-tests"
 BUILD_BUILTIN="sudo ./scripts/build.py build_builtins"
@@ -39,7 +38,12 @@ PLAY_SOUND="afplay /System/Library/Sounds/Submarine.aiff"
 
 RUN_EDITOR="(cd editor/;lein run)"
 BUILD_EDITOR="(cd editor/;lein init)"
-EDITOR="(cd editor/;lein init)"
+
+BUNDLE_BUILD="./scripts/bundle.py build  --platform=$2 --version=$3 --engine-artifacts=dynamo-home --skip-tests"
+BUNDLE_BUNDLE="./scripts/bundle.py bundle  --platform=$2 --version=$3 --engine-artifacts=dynamo-home"
+
+OPEN_FOLDER_MACOS="open ./editor/release"
+OPEN_FOLDER_UBUNTU="nautilus ./editor/release"
 
 FORCE="sudo chmod -R 777 ./"
 BUILD_MODULE="./scripts/submodule.sh x86_64-darwin $2 $3"
@@ -56,43 +60,32 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
-  --lein_install)
+  --lein)
     echo "Installing lein.."
     eval $COPY_LEIN
-    ;;
-  -F | --full )
-    start=$SECONDS
-    eval $BUILD_ENGINE
-    eval $BUILD_EDITOR
-    duration=$(( SECONDS - start ))
-    echo "====================================================="
-    echo "${GREEN}Finished build in ${duration} secs. ${NC}"
-    echo "====================================================="
-    eval $RUN_EDITOR
-    exit
     ;;
   -f | --fast )
     eval $SUB_MODULE
     eval $COPY_ENGINE_X8664
     exit
     ;;
-  -w | --waf )
+  --waf )
     eval $WAF_CONF
     exit
     ;;
-  -fo | --force )
+  --force )
     eval $FORCE
     exit
     ;;
-  -nf | --notify )
+  --notify )
     eval $PLAY_SOUND
     exit
     ;;
-  -s | --setup )
+  --setup )
     eval $SETUP
     exit
     ;;
-  -sh | --shell )
+  --shell )
     eval $SHELL_INTRO
     case $2 in
       mojave)
@@ -118,11 +111,11 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     esac
     exit
     ;;
-  -b | --editor )
-    eval $EDITOR
+  --editor )
+    eval $BUILD_EDITOR
     exit
     ;;
-  -cp| --copy )
+  --copy )
     echo "--------------------------------------------------"
     echo "COPY TO EDITOR ..."
     cp -r "${ENGINE_PATH}" "${EDITOR_PATH}"
@@ -130,63 +123,66 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     echo "--------------------------------------------------"
     exit
     ;;
-  -e | --engine )
+  --engine )
     eval $BUILD_ENGINE
     eval $COPY_ENGINE
     exit
     ;;
-  -m | --misc )
+  --misc )
     eval $BUILD_BOB
     eval $BUILD_BUILTIN
     exit
     ;;
-  -d | --doc )
+  --doc )
     eval $BUILD_DOC
     ;;
-  -D | --docker )
+  --docker )
     echo "Starting Docker App..."
     eval $RUN_DOCKER
     eval "read -t 20 -n 1"
     eval $EXPORT_DYNAMO_HOME
     eval $RUN_EXTENDER
     ;;
-  -B | --bundle )
+  --bundle )
     start=$SECONDS
-    eval $BUNDLE
+    echo "Start bundling Defold Editor.."
+    (cd "./editor";eval $BUNDLE_BUILD)
+    (cd "./editor";eval $BUNDLE_BUNDLE)
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      eval $OPEN_FOLDER_UBUNTU 
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+      eval $OPEN_FOLDER_MACOS
+    fi
     duration=$(( SECONDS - start ))
     echo "====================================================="
     echo "${GREEN}Finished bundling in ${duration} secs. ${NC}"
     echo "====================================================="
     exit
     ;;
-  -r | --run )
+  --run )
     eval $RUN_EDITOR
     exit
     ;;
-  -h | --help )
+  --help )
     echo "______________________________________________"
     echo "Shorthand to build Defold Engine"
     echo "@thetrung | July 2020"
     echo ""
     echo "__________________[COMMAND]___________________"
-    echo "sh cmd.sh --setup | -s : for environment setup"
-    echo "sh cmd.sh --shell | -sh: for shell x86_64-darwin | armv7-darwin | arm64..."
-    echo "sh cmd.sh --copy  | -cp: to copy <platform> dmengine -> editor"
-    echo "sh cmd.sh --engine| -e : --engine x86_64-darwin => to build macOS dmengine"
-    echo "sh cmd.sh --editor| -e : for building editor"
-    echo "sh cmd.sh --notify| -nf: play a sound from MacOS"
-    echo "sh cmd.sh --misc  | -m : for building bob + builtin"
-    echo "sh cmd.sh --doc   | -d : for building editor document"
-    echo "sh cmd.sh --docker| -D : to run Docker local server from /extender"
-    echo "sh cmd.sh --full  | -F : to build engine/editor + launch"
-    echo "sh cmd.sh --fast  | -f : -f <submodule-target-name>"
-    echo "sh cmd.sh --waf   | -w : --waf <platform> <path-to-module>"
-    echo "sh cmd.sh --force | -fo: enable submodule when 'Operation is not permitted'"
-    echo "sh cmd.sh --run   | -r : for running editor"
-    echo "sh cmd.sh --bundle| -B : for bundling editor into ./editor/release with given version"
-    echo "__________________[SHORTHAND]___________________"
-    echo "sh cmd.sh --cd_mojave      | -cdm : add defold path so you can just call: cd_defold"
-    echo "sh cmd.sh --cd_catalina    | -cdc : add defold path so you can just call: cd_defold"
+    echo "sh cmd.sh --setup  : for environment setup"
+    echo "sh cmd.sh --shell  : for shell x86_64-darwin | armv7-darwin | arm64..."
+    echo "sh cmd.sh --copy   : to copy <platform> dmengine -> editor"
+    echo "sh cmd.sh --engine : upto 3 args { x86_64-darwin --use-mp --skip-bob-light }"
+    echo "sh cmd.sh --editor : for building editor alone"
+    echo "sh cmd.sh --notify : play a sound on MacOS"
+    echo "sh cmd.sh --misc   : build bob + builtin"
+    echo "sh cmd.sh --doc    : build editor document"
+    echo "sh cmd.sh --docker : to run Docker local server from /extender"
+    echo "sh cmd.sh --fast   : -f taptic_engine"
+    echo "sh cmd.sh --waf    : --waf <platform> <path-to-module>"
+    echo "sh cmd.sh --force  : enable CLI when 'Operation is not permitted'"
+    echo "sh cmd.sh --run    : run editor"
+    echo "sh cmd.sh --bundle : bundle editor into ./editor/release with given platform & version"
     echo "______________________________________________"
     echo "You can also run each script separately as :"
     echo $SETUP
@@ -196,7 +192,7 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     echo $BUILD_ENGINE
     echo $BUILD_ENGINE_IOS_v7
     echo $BUILD_ENGINE_IOS_64
-    echo $EDITOR
+    echo $BUILD_EDITOR
     echo $PLAY_SOUND
     echo $BUILD_BOB
     echo $BUILD_BUILTIN
