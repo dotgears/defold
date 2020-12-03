5890bc66025b5b9d7d954c12b8ecac3376b5e496
Trung Vu <vptrung@yahoo.com>
2020-12-03 15:39:27 +0700

    sdks version fixed and merged

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -69,16 +69,16 @@ ANDROID_GCC_VERSION='4.9'
 ANDROID_64_NDK_API_VERSION='21' # Android 5.0
 EMSCRIPTEN_ROOT=os.environ.get('EMSCRIPTEN', '')
 
-IOS_SDK_VERSION="13.5"
-IOS_SIMULATOR_SDK_VERSION="13.5"
+IOS_SDK_VERSION="14.2"
+IOS_SIMULATOR_SDK_VERSION="14.2"
 # NOTE: Minimum iOS-version is also specified in Info.plist-files
 # (MinimumOSVersion and perhaps DTPlatformVersion)
 MIN_IOS_SDK_VERSION="8.0"
 
-OSX_SDK_VERSION="10.15"
+OSX_SDK_VERSION="11.0"
 MIN_OSX_SDK_VERSION="10.7"
 
-XCODE_VERSION="11.5"
+XCODE_VERSION="12.2"
 
 SDK_ROOT=os.path.join(os.environ['DYNAMO_HOME'], 'ext', 'SDKs')
 DARWIN_TOOLCHAIN_ROOT=os.path.join(SDK_ROOT,'XcodeDefault%s.xctoolchain' % XCODE_VERSION)
